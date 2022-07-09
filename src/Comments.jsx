db165474cdb02393aaa98608a81a0756dc141949
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-07-09 13:34:19 +0300

    Single Post
    Experiments with Redux, namely the ability to add comments, as well as their change and delete if necessary. In the next commit I'll try to work with a third-party API and understand Redux thunk.

---
+++ src/Comments.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { commentCreate } from "./redux/action";
+import { useDispatch, useSelector } from 'react-redux';
+import uniqid from 'uniqid';
+import SingleComment from "./SingleComment";
+
+
+function Comments (props) {
+    const [textComment, setTextComment] = useState('')
+    const comments = useSelector(state => {
+        const { commentsReducer } = state
+        return commentsReducer.comments
+    })
+    const dispatch = useDispatch();
+
+    const handleInput = (e) => {
+        setTextComment(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const id = uniqid()
+        dispatch(commentCreate(textComment, id));
+        setTextComment('')
+    }
+
+    return (
+        <div className="card-comments">
+            <form onSubmit={handleSubmit} className="comments-item-create">
+                <input type="text" value={textComment} onChange={handleInput} />
+                <input type="submit" hidden />
+            </form>
+            {!!comments.length && comments.map(result => {
+                return <SingleComment key={result.id} data={result}/>
+            })}
+        </div>
+    )
+}
+
+export default Comments;
